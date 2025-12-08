3999d8353fe9e16af0636a11d505afbf25f749a3
emblem-66 <143352594+emblem-66@users.noreply.github.com>
2025-12-08 11:56:06 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,13 +2,24 @@
 set -xeuo pipefail
 
 dnf install -y \
+    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm \
+
+dnf install -y --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' \
+    terra-release
+
+dnf install -y helium-browser-bin
+
+dnf install -y \
     ibm-plex* \
     adw-gtk3-theme \
 
-dnf remove -y \
-    google* \
+dnf install -y \
+    kitty \
+    pcmanfm \
+    wofi \
 
 dnf remove -y \
-    default-fonts* \
+    firefox* \
 
 rpm -qa | sort
