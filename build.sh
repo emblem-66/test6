691e34199cfb4e82cd0f611c439b206bbfe4acbd
emblem-66 <143352594+emblem-66@users.noreply.github.com>
2026-01-03 18:13:36 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,128 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 set -xeuo pipefail
+
+
+
+systemctl enable systemd-timesyncd
+systemctl enable systemd-resolved.service
+
+dnf -y install 'dnf5-command(config-manager)'
+
+dnf config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+dnf config-manager setopt tailscale-stable.enabled=0
+dnf -y install --enablerepo='tailscale-stable' tailscale
+
+systemctl enable tailscaled
+
+dnf -y remove \
+  console-login-helper-messages \
+  chrony \
+  sssd* \
+  qemu-user-static* \
+  toolbox
+
+dnf -y install greetd
+
+# These were manually picked out from a Bluefin comparison with `rpm -qa --qf="%{NAME}\n" `
+dnf -y install \
+  -x PackageKit* \
+  NetworkManager \
+  NetworkManager-adsl \
+  NetworkManager-bluetooth \
+  NetworkManager-config-connectivity-fedora \
+  NetworkManager-libnm \
+  NetworkManager-openconnect \
+  NetworkManager-openvpn \
+  NetworkManager-strongswan \
+  NetworkManager-ssh \
+  NetworkManager-ssh-selinux \
+  NetworkManager-vpnc \
+  NetworkManager-wifi \
+  NetworkManager-wwan \
+  alsa-firmware \
+  alsa-sof-firmware \
+  alsa-tools-firmware \
+  atheros-firmware \
+  audispd-plugins \
+  audit \
+  brcmfmac-firmware \
+  cifs-utils \
+  cups \
+  cups-pk-helper \
+  dymo-cups-drivers \
+  firewalld \
+  fprintd \
+  fprintd-pam \
+  fuse \
+  fuse-common \
+  fwupd \
+  gum \
+  gvfs-archive \
+  gvfs-mtp \
+  gvfs-nfs \
+  gvfs-smb \
+  hplip \
+  hyperv-daemons \
+  ibus \
+  ifuse \
+  intel-audio-firmware \
+  iwlegacy-firmware \
+  iwlwifi-dvm-firmware \
+  iwlwifi-mvm-firmware \
+  jmtpfs \
+  kernel-modules-extra \
+  libcamera{,-{v4l2,gstreamer,tools}} \
+  libimobiledevice \
+  libimobiledevice-utils \
+  libratbag-ratbagd \
+  man-db \
+  man-pages \
+  mobile-broadband-provider-info \
+  mt7xxx-firmware \
+  nxpwireless-firmware \
+  open-vm-tools \
+  open-vm-tools-desktop \
+  openconnect \
+  pam_yubico \
+  pcsc-lite \
+  plymouth \
+  plymouth-system-theme \
+  printer-driver-brlaser \
+  ptouch-driver \
+  qemu-guest-agent \
+  realtek-firmware \
+  rsync \
+  spice-vdagent \
+  steam-devices \
+  switcheroo-control \
+  system-config-printer-libs \
+  system-config-printer-udev \
+  systemd-container \
+  systemd-oomd-defaults \
+  tiwilink-firmware \
+  tuned \
+  tuned-ppd \
+  unzip \
+  usb_modeswitch \
+  uxplay \
+  vpnc \
+  whois \
+  wireguard-tools \
+  zram-generator-defaults
+
+systemctl enable auditd
+systemctl enable firewalld
+
+systemctl enable greetd
+
+
 
 ### Config files
 # Terra
 #curl -sS --create-dirs -o /etc/yum.repos.d/terra.repo https://github.com/terrapkg/subatomic-repos/raw/main/terra.repo
 # repo - tailscale
-curl -sS --create-dirs -o /etc/yum.repos.d/_tailscale.repo https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+#curl -sS --create-dirs -o /etc/yum.repos.d/_tailscale.repo https://pkgs.tailscale.com/stable/fedora/tailscale.repo
 # repo - morewaita icons
 #curl -sS --create-dirs -o /etc/yum.repos.d/_morewaita.repo https://copr.fedorainfracloud.org/coprs/trixieua/morewaita-icon-theme/repo/fedora-$(rpm -E %fedora)/trixieua-morewaita-icon-theme-fedora-$(rpm -E %fedora).repo
 # repo -ghostty
@@ -59,8 +176,6 @@ systemctl enable cockpit.socket
 rpm -qa | sort
 
 dnf install -y steam
-
-dnf search mesa
 
 dnf install -y terra-release-mesa
 
