7f9a58d19bd922854242678068a88fdf490a389d
emblem-66 <143352594+emblem-66@users.noreply.github.com>
2026-04-22 18:29:28 +0200

    Create build-niri.sh

---
+++ build-niri.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -xeuo pipefail
+
+dnf install -y 'dnf5-command(config-manager)'
+
+dnf install -y glibc-minimal-langpack
+dnf remove -y glibc-all-langpacks
+
+# Debloat
+dnf remove -y qemu-user-static* sssd*
+# Tailscale
+dnf config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+dnf config-manager setopt tailscale-stable.enabled=0
+dnf install -y --enablerepo='tailscale-stable' tailscale
+systemctl enable tailscaled
+systemctl enable sshd.service
+# Just
+dnf install -y just
+# Niri & DMS
+dnf copr enable -y yalter/niri
+dnf install -y \
+    niri \
+    material-symbols-fonts \
+    adw-gtk3-theme
+# 
+dnf install -y nautilus
+# Flatpak
+dnf install -y flatpak
+# Cockpit
+dnf install -y cockpit
+systemctl enable cockpit.socket
+# Firewall
+dnf install -y firewalld
+systemctl enable firewalld.service
+# Greetd
+#systemctl enable greetd
+#systemctl set-default graphical.target
+dnf autoremove -y
