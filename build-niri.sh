dc93231ebb4fbc1f31b2230c46fe6848fd346156
emblem-66 <143352594+emblem-66@users.noreply.github.com>
2026-04-22 18:37:27 +0200

    Update build-niri.sh

--- build-niri.sh
+++ build-niri.sh
@@ -3,8 +3,7 @@
 set -xeuo pipefail
 
 dnf install -y 'dnf5-command(config-manager)'
-
-dnf install -y glibc-minimal-langpack
+dnf install -y glibc-minimal-langpack glibc-langpack-en glibc-langpack-cs
 dnf remove -y glibc-all-langpacks
 
 # Debloat
@@ -34,6 +33,123 @@ systemctl enable cockpit.socket
 dnf install -y firewalld
 systemctl enable firewalld.service
 # Greetd
-#systemctl enable greetd
-#systemctl set-default graphical.target
-dnf autoremove -y
+systemctl enable greetd
+systemctl set-default graphical.target
+
+
+
+dnf -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release{,-extras,-mesa} 
+#dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+#dnf -y config-manager setopt "*terra*".priority=3 #"*terra*".exclude="nerd-fonts topgrade scx-tools scx-scheds steam python3-protobuf zlib-devel"
+#dnf -y config-manager setopt "terra-mesa".enabled=false
+#dnf -y config-manager setopt "*rpmfusion*".priority=5 "*rpmfusion*".exclude="mesa-* steam*"
+#dnf -y config-manager setopt "*fedora*".exclude="mesa-*" # kernel-core-* kernel-modules-* kernel-uki-virt-*"
+
+# --setopt=install_weak_deps=False
+
+dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf config-manager setopt fedora-cisco-openh264.enabled=1
+
+#dnf config-manager setopt fedora-cisco-openh264.enabled=0
+
+#dnf config-manager addrepo --from-repofile=https://github.com/terrapkg/subatomic-repos/raw/main/terra.repo
+#sed -i '0,/enabled=0/s/enabled=0/enabled=1/' /etc/yum.repos.d/terra-mesa.repo
+#dnf config-manager setopt terra.enabled=1
+#dnf install -y --enablerepo='terra' terra-release
+#dnf install -y --enablerepo='terra' terra-release-extras
+#dnf install -y --enablerepo='terra' terra-release-mesa
+#dnf install -y --enablerepo='terra' terra-release-nvidia
+#dnf install -y --enablerepo='terra' terra-release-multimedia
+
+#dnf install -y terra-release*
+#dnf install -y terra-release
+#dnf install -y terra-release-extras
+#dnf install -y terra-release-mesa
+#dnf install -y terra-release-nvidia
+#dnf install -y terra-release-multimedia
+
+#dnf swap mesa-va-drivers-freeworld mesa-va-drivers
+#dnf install -y mangowm noctalia-shell
+
+# Docker
+dnf config-manager addrepo --from-repofile=https://download.docker.com/linux/fedora/docker-ce.repo
+dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+dnf copr enable -y che/nerd-fonts
+dnf config-manager setopt copr:copr.fedorainfracloud.org:che:nerd-fonts.enabled=0
+dnf install -y --enablerepo='copr:copr.fedorainfracloud.org:che:nerd-fonts' nerd-fonts
+
+# Tailscale
+dnf config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+dnf config-manager setopt tailscale-stable.enabled=0
+dnf install -y --enablerepo='tailscale-stable' tailscale
+#dnf install -y --repofrompath=tailscale-stable,https://pkgs.tailscale.com/stable/fedora/tailscale.repo tailscale
+#curl -fsSL --create-dirs -o /etc/yum.repos.d/tailscale.repo \
+#https://raw.githubusercontent.com/emblem-66/bootc-config/refs/heads/main/system_files/etc/yum.repos.d/tailscale.repo
+#dnf install -y tailscale
+
+# Adwaita & Morewaita
+dnf copr enable -y trixieua/morewaita-icon-theme
+dnf config-manager setopt copr:copr.fedorainfracloud.org:trixieua:morewaita-icon-theme.enabled=0
+dnf install -y --enablerepo='copr:copr.fedorainfracloud.org:trixieua:morewaita-icon-theme' morewaita-icon-theme
+#dnf install -y adw-gtk3-theme morewaita-icon-theme
+#curl -fsSL --create-dirs -o /etc/yum.repos.d/morewaita.repo \
+#https://raw.githubusercontent.com/emblem-66/bootc-config/refs/heads/main/system_files/etc/yum.repos.d/morewaita.repo
+#dnf install -y adw-gtk3-theme morewaita-icon-theme
+
+dnf copr enable -y peterwu/rendezvous
+dnf config-manager setopt copr:copr.fedorainfracloud.org:peterwu:rendezvous.enabled=0
+dnf install -y --enablerepo='copr:copr.fedorainfracloud.org:peterwu:rendezvous' bibata-cursor-themes
+
+dnf install -y papirus-icon-theme adw-gtk3-theme 
+
+# MergerFS
+dnf copr enable -y errornointernet/mergerfs
+dnf config-manager setopt copr:copr.fedorainfracloud.org:errornointernet:mergerfs.enabled=0
+dnf install -y --enablerepo='copr:copr.fedorainfracloud.org:errornointernet:mergerfs' mergerfs
+#dnf install -y mergerfs
+#curl -fsSL --create-dirs -o /etc/yum.repos.d/mergerfs.repo \
+#https://raw.githubusercontent.com/emblem-66/bootc-config/refs/heads/main/system_files/etc/yum.repos.d/mergerfs.repo
+#dnf install -y mergerfs
+
+
+dnf install -y greetd tuigreet
+
+dnf copr enable -y yalter/niri
+dnf config-manager setopt copr:copr.fedorainfracloud.org:yalter:niri.enabled=0
+dnf install -y --enablerepo='copr:copr.fedorainfracloud.org:yalter:niri' niri
+
+dnf install -y \
+    fuzzel \
+    alacritty \
+    SwayNotificationCenter \
+    swaybg \
+    swayidle \
+    swaylock \
+    waybar \
+    mako \
+    grim \
+    slurp \
+
+
+# File system
+dnf install -y \
+    smartmontools \
+    btrfsd \
+    btrfsmaintenance \
+
+# Screen brightness
+dnf install -y ddcutil
+
+# Debug
+dnf install -y evtest
+
+# Update tweaks
+sed -i 's|^ExecStart=.*|ExecStart=/usr/bin/bootc update --quiet|' /usr/lib/systemd/system/bootc-fetch-apply-updates.service
+sed -i 's|#AutomaticUpdatePolicy.*|AutomaticUpdatePolicy=stage|' /etc/rpm-ostreed.conf
+sed -i 's|#LockLayering.*|LockLayering=true|' /etc/rpm-ostreed.conf
+
+# Passwordless sudo
+echo "%wheel ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/90-passwordless-sudo
+cat /etc/sudoers.d/90-passwordless-sudo
+chmod 0440 /etc/sudoers.d/90-passwordless-sudo
